445b2f4c75254c4f9cd5998e9a1b30d3bc19486c
jonathanreimo <jon.mauricio@outlook.com>
2023-02-24 19:48:34 -0600

    Fetch de API de BikeIndex para bicicletas.html y Fetch de array (arreglo) JS vainilla para index.html

--- js/vistas.js
+++ js/vistas.js
@@ -1,9 +1,11 @@
+  //Fetch de vista de seccion header
 fetch('../header.html')
   .then(response => response.text())
   .then(data => {
     document.querySelector('#header').innerHTML = data;
   });
 
+//Fetch de vista de seccion header
   fetch('../footer.html')
   .then(response => response.text())
   .then(data => {
@@ -16,6 +18,37 @@ fetch('../header.html')
     /* Copy año */
     document.getElementById("year").innerHTML = new Date().getFullYear();
 	  });
+
+
+    // Fetch de API de BikeIndex para bicicletas.html
+  fetch('https://bikeindex.org:443/api/v3/bikes_search?page=1&per_page=3')
+  .then(response => response.json())
+  .then(data => {
+    const bikeList = document.getElementById("bici-list");
+    data.bikes.forEach(bike => {
+      //const bikeName = document.createElement('p');
+      const bikeName = document.createElement('h1');
+      bikeName.innerHTML = `<div class="row"><div class="col-md-6"><div class="box_main"><div class="image_2"><img src="images/img-2.png"></div></div></div><div class="col-md-6"><h1 class="cycles_text">${bike.title}</h1><p class="lorem_text">Rodadas para todo terreno, en montaña y ciudad</p><div class="btn_main"><h4 class="price_text">Precio <span style=" color: #f7c17b">$</span> <span style=" color: #325662">200</span></h4></div></div></div>`;
+      bikeList.appendChild(bikeName);
+    });
+  })
+  .catch(error => console.error(error));
+
+  // //Fetch de array (arreglo) JS vainilla para index.html
+const arrayHTML = ['<div class="cycle_section_2 layout_padding"><div class="row"><div class="col-md-6"><div class="box_main"><div class="image_2"><img src="images/img-2.png"></div></div></div><div class="col-md-6"><h1 class="cycles_text">Mejor</h1><p class="lorem_text">Rodadas para todo terreno, en montaña y ciudad</p><div class="btn_main"><h4 class="price_text">Precio <span style=" color: #f7c17b">$</span> <span style=" color: #325662">200</span></h4></div></div></div></div>', '<div class="cycle_section_3 layout_padding"> <div class="row"> <div class="col-md-6"> <h1 class="cycles_text">Nueva</h1> <p class="lorem_text">Un cambio de camino y ahora un cambio de cyclo, nueva para tus nuevas ciudades</p> <div class="btn_main"> <h4 class="price_text">Precio <span style=" color: #f7c17b">$</span> <span style=" color: #325662">200</span></h4> </div> </div> <div class="col-md-6"> <div class="box_main_3"> <h6 class="number_text_2">02</h6> <div class="image_2"><img src="images/img-3.png"></div> </div> </div> </div> </div>', ' <div class="cycle_section_2 layout_padding"> <div class="row"> <div class="col-md-6"> <div class="box_main_3"> <h6 class="number_text_2">03</h6> <div class="image_2"><img src="images/img-4.png"></div> </div> </div> <div class="col-md-6"> <h1 class="cycles_text">Clásico <br>Cyclo</h1> <p class="lorem_text">Donde siempre te mueves, se moverá cyclo</p> <div class="btn_main"> <h4 class="price_text">Precio <span style=" color: #f7c17b">$</span> <span style=" color: #325662">200</span></h4> </div> </div> </div> </div>'];
+
+// Seleccionamos el contenedor donde queremos agregar los elementos
+const contenedor = document.querySelector(".contenedorp");
+
+// Iteramos sobre el array de cadenas HTML y creamos un nuevo div por cada una
+arrayHTML.forEach(html => {
+  const div = document.createElement("div"); // Creamos un nuevo elemento <div>
+  div.innerHTML = html; // Añadimos el HTML al elemento <div>
+  contenedor.appendChild(div); // Agregamos el elemento <div> al contenedor
+});
+
+
+
 
 // Seleccionar el formulario y agrega un evento de "submit" a él
 const formulario = document.querySelector('#mi-formulario');
